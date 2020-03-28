deea68db4d3ea13870abdb736025cc3ff85f6e16
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2020-03-29 00:41:02 +0100

    Made gift component completely dynamic

--- src/components/home/Gift.vue
+++ src/components/home/Gift.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="gift">
+  <div :style="cssVars" class="gift">
     <b-container>
-      <p class="title">Gifts for her</p>
+      <p class="title">{{gifts.title}}</p>
       <b-row>
-        <GiftItem v-for="gift in gifts" v-bind:key="gift.index" v-bind:gift="gift" />
+        <GiftItem v-for="gift in gifts.gifts" v-bind:key="gift.index" v-bind:gift="gift" />
       </b-row>
     </b-container>
   </div>
@@ -17,25 +17,35 @@ export default {
     GiftItem
   },
   props: {
-    gifts: Array
+    gifts: Object
+  },
+  computed: {
+    cssVars() {
+      return {
+        "--background-color": this.gifts.backgroundColor,
+        "--background-image": `url('${this.gifts.backgroundImage}')`,
+        "--left-sector": `url('${this.gifts.leftSector}')`,
+        "--right-sector": `url('${this.gifts.rightSector}')`
+      };
+    }
   }
 };
 </script>
 
 <style scoped lang="less">
 .gift {
-  background-color: #fff6f9;
-  background-image: url("../../assets/img/herbackground.png");
   padding-bottom: 7rem;
   padding-top: 7rem;
   position: relative;
+  background-image: var(--background-image);
+  background-color: var(--background-color);
   &::before {
     content: "";
     position: absolute;
     top: 0;
     left: 0;
     z-index: 99;
-    background-image: url("../../assets/img/lefther.png");
+    background-image: var(--left-sector);
     background-repeat: no-repeat;
     background-size: contain;
     width: 18vw;
@@ -47,7 +57,7 @@ export default {
     top: 0;
     right: 0;
     z-index: 99;
-    background-image: url("../../assets/img/righther.png");
+    background-image: var(--right-sector);
     background-repeat: no-repeat;
     background-size: contain;
     width: 18vw;
